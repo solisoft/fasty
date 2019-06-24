293c094223a1f76729c21c80ddb4fbffe5b82cf1
Olivier Bonnaure <olivier@solisoft.net>
2019-06-24 14:17:43 +0200

    Pages

--- foxxy/foxx/cruds/APP/models/pages.js
+++ foxxy/foxx/cruds/APP/models/pages.js
@@ -44,16 +44,14 @@ const model = function () {
     model: [
       { r: true, c: "3-4", n: "name", t: "string", j: joi.string().required(), l: "Name", tr: true },
       { r: false, c: "1-4", n: "layout_id", t: "list", j: joi.string().required(), l: "Layout", d: layouts },
-      { r: true, c: "1-3", n: "slug", t: "string", j: joi.string().required(), l: "Slug", tr: true },
-      { r: false, c: "1-3", n: "online", t: "boolean", j: joi.number().integer(), l: "Online?" },
-      { r: false, c: "1-3", n: "published_at", t: "date", j: joi.date().format('YYYY-MM-DD').raw().required(), l: "Published_at" },
+      { r: true, c: "1-1", n: "slug", t: "string", j: joi.string().required(), l: "Slug", tr: true },
       { r: true, c: "1-1", n: "html", t: "html", j: joi.any(), l: "Content", tr: true },
       { r: true, c: "1-1", n: "description", t: "text", j: joi.any(), l: "Description", tr: true },
 
       { r: true, c: "1-1", n: "og_title", t: "string", j: joi.any(), l: "og:title", tr: true },
       { r: true, c: "1-1", n: "og_img", t: "string", j: joi.any(), l: "og:img", tr: true },
       { r: true, c: "1-1", n: "og_type", t: "string", j: joi.any(), l: "og:type", tr: true },
-      { r: true, c: "1-1", n: "og_aql", t: "text", j: joi.any(), l: "og:aql", tr: true },
+      { r: true, c: "1-1", n: "og_aql", t: "code:aql", j: joi.any(), l: "og:aql", tr: true },
 
     ],
     columns: [
