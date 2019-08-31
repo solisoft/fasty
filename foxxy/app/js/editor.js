60f285bc691f7f5fedd07ff683e57c542ed377b4
Olivier Bonnaure <olivier@solisoft.net>
2019-08-31 20:22:07 +0300

    Fix pictures srcset orders

--- foxxy/app/js/editor.js
+++ foxxy/app/js/editor.js
@@ -224,12 +224,12 @@
                     success: function (data) {
                       setTimeout(function () {
                         var picture = '<picture>'
-                        picture += '<source media="(max-width: 480px)" srcset="https://resize.ovh/r/' + data.filename + '/480">'
                         picture += '<source media="(max-width: 480px)" srcset="https://resize.ovh/r/' + data.filename + '/480/webp" type="image/webp">'
-                        picture += '<source media="(max-width: 799px)" srcset="https://resize.ovh/r/' + data.filename + '/799">'
+                        picture += '<source media="(max-width: 480px)" srcset="https://resize.ovh/r/' + data.filename + '/480">'
                         picture += '<source media="(max-width: 799px)" srcset="https://resize.ovh/r/' + data.filename + '/799/webp" type="image/webp">'
-                        picture += '<source media="(min-width: 800px)" srcset="https://resize.ovh/o/' + data.filename + '">'
+                        picture += '<source media="(max-width: 799px)" srcset="https://resize.ovh/r/' + data.filename + '/799">'
                         picture += '<source media="(min-width: 800px)" srcset="https://resize.ovh/o/' + data.filename + '/webp" type="image/webp">'
+                        picture += '<source media="(min-width: 800px)" srcset="https://resize.ovh/o/' + data.filename + '">'
                         picture += '<img src="https://resize.ovh/o/' + data.filename + '">'
                         picture += '</picture>'
                         $(el).html(picture)
