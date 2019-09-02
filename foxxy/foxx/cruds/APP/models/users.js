c6cd1c5d0adace2f2baf5a873581dd9e6efe6a5d
Olivier Bonnaure <olivier@solisoft.net>
2019-09-02 15:25:44 +0300

    Fix Users

--- foxxy/foxx/cruds/APP/models/users.js
+++ foxxy/foxx/cruds/APP/models/users.js
@@ -46,7 +46,7 @@ const model = function() {
       { r: false, c: "1-2", n: "ln", t: "string", j: "joi.string().required()", l: "Last Name" },
       { r: true, c: "1-3", n: "username", t: "string", j: "joi.string().required()", l: "Email" },
       { r: false, c: "1-3", n: "role", t: "string", j: "joi.string().required()", l: "Role" },
-      { r: false, c: "1-3", n: "a", t: "boolean", j: joi.number().integer(), l: "Active?" },
+      { r: false, c: "1-3", n: "a", t: "boolean", j: "joi.number().integer()", l: "Active?" },
       { r: true, c: "1-2", n: "password", t: "password", j: "joi.any()", l: "Password" },
       { r: false, c: "1-2", n: "password_confirmation", t: "password_confirmation", j: "joi.any().valid(joi.ref('password')).options({ language: { any: { allowOnly: 'must match password' } } })", l: "Password confirmation" },
       { r: true, c: "1-1", n: "image", t: "image", j: "joi.string()", l: "Avatar" },
