ed907cc05216f3bd1e8981a7c337cacf4d68ee86
Olivier Bonnaure <olivier@solisoft.net>
2020-01-21 22:46:06 +0100

    Fix layout JS order

--- foxxy/foxx/cruds/APP/models/layouts.js
+++ foxxy/foxx/cruds/APP/models/layouts.js
@@ -46,10 +46,10 @@ const model = function() {
       { r: false, c: "1-2", n: "page_builder", t: "list", j: "joi.string().required()", l: "Page Builder", d: partials },
       { r: true, c: "1-1", n: "html", t: "code:html", j: "joi.any()", l: "HTML" },
       { tab: "CSS" },
-      { r: true, c: "1-1", n: "i_css", t: "code:text", j: "joi.any()", l: "Vendors CSS" },
+      { r: true, c: "1-1", n: "i_css", t: "code:scss", j: "joi.any()", l: "Vendors CSS" },
       { r: true, c: "1-1", n: "scss", t: "code:scss", j: "joi.any()", l: "CSS/Sass" },
       { tab: "JS" },
-      { r: true, c: "1-1", n: "i_js", t: "code:text", j: "joi.any()", l: "Vendors JS" },
+      { r: true, c: "1-1", n: "i_js", t: "code:javascript", j: "joi.any()", l: "Vendors JS" },
       { r: true, c: "1-1", n: "javascript", t: "code:javascript", j: "joi.any()", l: "JS", endtab: true }
 
     ],
