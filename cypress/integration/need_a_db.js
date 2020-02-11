f280a426059eef1261ae363dbd8084271bd60e2f
Olivier Bonnaure <olivier@solisoft.net>
2020-02-11 22:28:32 +0100

    Update circleci

--- cypress/integration/need_a_db.js
+++ cypress/integration/need_a_db.js
@@ -1,11 +1,7 @@
-import { utils } from "mocha";
-
 var host = 'http://demo.127.0.0.1.xip.io:8080'
 describe('Static page test', function () {
-
   it('Test Need a DB page', function () {
-    cy.visit('/need_a_db')
+    cy.visit(hpst + '/need_a_db')
     cy.get('body').should('contain', 'Your account is not yet configured')
   })
-
 })
