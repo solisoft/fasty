4cd6a535e849893ed053d1ff2d88a1afff18f13d
Olivier Bonnaure <olivier@solisoft.net>
2020-02-13 12:38:29 +0100

    run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+yarn
 node cypress/setup_test.js
 cd foxxy
 foxxy upgrade settings --server fasty --database db_test
 foxxy upgrade --server fasty --database db_test
 cd ..
-cypress run
+./node_modules/cypress/bin/cypress run
