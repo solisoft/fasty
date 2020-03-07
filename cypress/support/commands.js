d00d84c2d871341939c942e8f048b7b72a8f8786
Olivier BONNAURE <olivier@solisoft.net>
2020-03-07 19:50:44 +0100

    Test splat & tr shortcuts

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -20,6 +20,36 @@ Cypress.Commands.add("login", () => {
   cy.visit(host + 'index.html');
   cy.url().should('eq', host + 'index.html#welcome');
 })
+
+Cypress.Commands.add("create_page", (slug, html) => {
+  cy.visit(host)
+  cy.visit(host + '/#pages')
+  cy.get('div[data-is="pages"]').contains('New page').click()
+  cy.get('#name').type(slug);
+  cy.get('#slug').type(slug);
+  cy.get('#raw_html').then(elem => { elem.val(html) })
+  cy.get('input[type="submit"]').click();
+})
+
+Cypress.Commands.add("create_aql", (slug, aql) => {
+  cy.visit(host)
+  cy.visit(host + '/#aqls')
+  cy.get('div[data-is="aqls"]').contains('New aql').click()
+  cy.get('#slug').type(slug);
+  cy.get('#aql').then(elem => { elem.val(aql) })
+  cy.get('input[type="submit"]').click();
+})
+
+Cypress.Commands.add("create_partial", (slug, html) => {
+  cy.visit(host)
+  cy.visit(host + '/#partials')
+  cy.get('div[data-is="partials"]').contains('New partial').click()
+  cy.get('#name').type(slug);
+  cy.get('#slug').type(slug);
+  cy.get('#html').then(elem => { elem.val(html) })
+  cy.get('input[type="submit"]').click();
+})
+
 //
 //
 // -- This is a child command --
