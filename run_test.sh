c3a4458a25be93e3bacf1a214aea72b69cd931c4
Olivier BONNAURE <olivier@solisoft.net>
2020-03-08 13:28:39 +0100

    Test with 127.0.0.1 for tests ...

--- run_test.sh
+++ run_test.sh
@@ -5,4 +5,4 @@ cd foxxy
 foxxy upgrade settings --server fasty --database db_test
 foxxy upgrade --server fasty --database db_test
 cd ..
-./node_modules/cypress/bin/cypress run -b chrome --headless
+./node_modules/cypress/bin/cypress open
