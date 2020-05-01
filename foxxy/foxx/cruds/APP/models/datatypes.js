d1496466356c48daf6a8963158da09cd8080fd29
Olivier BONNAURE <olivier@solisoft.net>
2020-05-01 10:24:03 +0200

    Datatypes : deployable?

--- foxxy/foxx/cruds/APP/models/datatypes.js
+++ foxxy/foxx/cruds/APP/models/datatypes.js
@@ -44,7 +44,8 @@ const model = function() {
     model: [
       { r: true, c: "1-3", n: "name", t: "string", j: "joi.string().required()", l: "Name" },
       { r: false, c: "1-3", n: "slug", t: "string", j: "joi.string().required()", l: "Slug" },
-      { r: false, c: "1-3", n: "synchronizable", t: "boolean", j: "joi.number().integer()", l: "Synchronizable?" },
+      { r: false, c: "1-6", n: "synchronizable", t: "boolean", j: "joi.number().integer()", l: "Synchronizable?" },
+      { r: false, c: "1-6", n: "deployable", t: "boolean", j: "joi.number().integer()", l: "Deployable?" },
       { r: true, c: "1-1", n: "javascript", t: "code:json", j: "joi.string().required()", l: "JSON definition" }
 
     ],
