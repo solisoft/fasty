94c09b945a8a237b7159bc5cc184423a85e2241c
Olivier BONNAURE <olivier@solisoft.net>
2020-12-29 11:16:14 +0100

    Remove xip.io dependency in tests

--- cypress/integration/admin/auth.js
+++ cypress/integration/admin/auth.js
@@ -1,6 +1,6 @@
 import { utils } from "mocha";
 
-var host = 'http://test.127.0.0.1.xip.io:8080'
+var host = 'http://test.localhost:8080'
 describe('Auth', function () {
 
   beforeEach(function () {
